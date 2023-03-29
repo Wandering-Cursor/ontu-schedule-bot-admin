742e163be683bf1b01967e6a8fa9e55c030d2c82
Pavlo Pohorieltsev <667strets@gmail.com>
2023-03-29 19:08:49 +0300

    Add logging

--- admin_site/admin_site/settings.py
+++ admin_site/admin_site/settings.py
@@ -136,3 +136,22 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
