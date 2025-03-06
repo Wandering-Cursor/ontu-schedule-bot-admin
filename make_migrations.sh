0290d657e941f03fe80ab94fbb8f7096a410346f
Pavlo Pohorieltsev <49622129+makisukurisu@users.noreply.github.com>
2025-03-06 16:35:59 +0000

    Remove unused files

--- make_migrations.sh
+++
@@ -1,5 +0,0 @@
-python3.11 admin_site/manage.py makemigrations
-python3.11 admin_site/manage.py migrate
-python3.11 admin_site/manage.py createsuperuser \
-    --username=test \
-    --email=test@example.com
