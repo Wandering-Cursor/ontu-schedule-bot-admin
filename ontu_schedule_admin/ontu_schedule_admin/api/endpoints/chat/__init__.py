da0d0681644974005a64018c0b6aa0ec769fabee
Pavlo Pohorieltsev <49622129+makisukurisu@users.noreply.github.com>
2026-02-16 15:03:41 +0000

    feat: Add message campagin API support

--- ontu_schedule_admin/ontu_schedule_admin/api/endpoints/chat/__init__.py
+++ ontu_schedule_admin/ontu_schedule_admin/api/endpoints/chat/__init__.py
@@ -4,11 +4,12 @@ This endpoint module contains requests from authenticated (via chat) users.
 It allows to easily get schedule information for whatever subscription the user has.
 """
 
-from . import bulk, chat, schedule, subscription
+from . import bulk, chat, message_campagin, schedule, subscription
 
 __all__ = [
     "bulk",
     "chat",
+    "message_campagin",
     "schedule",
     "subscription",
 ]
---
+++ ontu_schedule_admin/ontu_schedule_admin/api/serializers/message_campaign.py
@@ -0,0 +1,32 @@
+from main.models.message_campaign import MessageCampaign
+from rest_framework import serializers
+
+from ontu_schedule_admin.api.serializers.chat import ChatSerializer
+
+
+class MessageCampaignSerializer(serializers.ModelSerializer):
+    recipients = ChatSerializer(
+        many=True,
+        read_only=True,
+    )
+
+    def to_representation(self, instance):  # noqa: ANN001, ANN201
+        data = super().to_representation(instance)
+
+        if chat_id := self.context.get("X-Chat-ID"):
+            data["recipients"] = [
+                item for item in data["recipients"] if item["platform_chat_id"] == chat_id
+            ]
+
+        return data
+
+    class Meta:
+        model = MessageCampaign
+        fields = [
+            "uuid",
+            "name",
+            "payload",
+            "recipients",
+            "created_at",
+            "updated_at",
+        ]
